d33fb19d5f93e95e08c072302576d436c15c3dc4
Artsiusheuski <i.artsiusheuski@gmail.com>
2022-09-19 18:23:50 +0200

    for_react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <section className="wrapper">
+      <h1 className="title">SPORT</h1>
+      <div>My test</div>
+    </section>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+let mode = "development";
+let target = "web";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
+}
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "[name].[contenthash].css",
+  }),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+];
+
+if (process.env.SERVE) {
+  plugins.push(new ReactRefreshWebpackPlugin());
+}
+
+module.exports = {
+  mode,
+  target,
+  plugins,
+  devtool: "source-map",
+  entry: "./src/index.js",
+  devServer: {
+    port: 3000,
+    open: true, //auto-downloads
+    static: "./dist",
+    hot: true,
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      { test: /\.html$/i, loader: "html-loader" },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === "production" ? "asset" : "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+};
