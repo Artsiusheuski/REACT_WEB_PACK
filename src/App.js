100084d2f30371992ea466b597f671a4ce43e492
Artsiusheuski <i.artsiusheuski@gmail.com>
2022-12-12 20:03:33 +0100

    5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 function App() {
   return (
-    <section className="wrapper">
-      <h1 className="title">SPORT</h1>
-      <div>My test</div>
-    </section>
+    <>
+      {" "}
+      <section className="wrapper">
+        <h1 className="title">SPORT</h1>
+        <div>My test</div>
+      </section>
+    </>
   );
 }
 
